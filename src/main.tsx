e56f2b9e290d52133fdc887e4a0472ef6fa03735
amjad <amjadshadid51@gmail.com>
2024-05-26 17:52:49 +0300

    add RM file

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.tsx'
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { Routes, Route, HashRouter} from "react-router-dom";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Preview from "./pages/Preview";
@@ -11,14 +11,14 @@ import {ToastProvider} from "./components/Toast";
 ReactDOM.render(
   <React.StrictMode>
     <ToastProvider>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/login" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/preview/:userId" element={<Preview />} />
           <Route path="/" element={<App />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </ToastProvider>
   </React.StrictMode>,
   document.getElementById('root')
