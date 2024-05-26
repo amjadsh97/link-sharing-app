93c0aa97330b173c22490b765f6251b2c05d9bcc
amjad <amjadshadid51@gmail.com>
2024-05-26 18:02:01 +0300

    add some fixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,31 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import App from './App.tsx'
-import { Routes, Route, HashRouter} from "react-router-dom";
+import {Routes, Route, BrowserRouter} from "react-router-dom";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Preview from "./pages/Preview";
 import {ToastProvider} from "./components/Toast";
+import ReactDOM from 'react-dom/client';
 
 
-ReactDOM.render(
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
   <React.StrictMode>
     <ToastProvider>
-      <HashRouter>
+      <BrowserRouter>
         <Routes>
           <Route path="/login" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/preview/:userId" element={<Preview />} />
           <Route path="/" element={<App />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </ToastProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+
+  </React.StrictMode>
 );
+
+
